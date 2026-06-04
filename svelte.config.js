7ed6e375ec63f0eedeebb030ffbdbca38018ac7b
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2026-06-04 09:11:17 -0700

    chore: sync svelte config from @reddoorla/maintenance

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,7 @@
+import { createSvelteConfig } from "@reddoorla/maintenance/configs/svelte";
 import adapter from "@sveltejs/adapter-netlify";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-  compilerOptions: {
-    warningFilter: (warning) =>
-      warning.code !== "element_invalid_self_closing_tag",
-  },
-  kit: {
-    adapter: adapter({
-      edge: false,
-      split: false,
-    }),
-    alias: {
-      $components: "src/lib/components",
-      "$components/*": "src/lib/components/*",
-      $utils: "src/lib/utils",
-      "$utils/*": "src/lib/utils/*",
-      $stores: "src/lib/stores",
-      "$stores/*": "src/lib/stores/*",
-      $assets: "src/lib/assets",
-      "$assets/*": "src/lib/assets/*",
-    },
-  },
-  preprocess: vitePreprocess(),
-};
-
-export default config;
+export default createSvelteConfig({
+  kit: { adapter: adapter({ edge: false, split: false }) },
+});
